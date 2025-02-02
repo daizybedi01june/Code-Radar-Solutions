b5caeeeb43119e7eaafb6378730d70239ea17c21
daizybedi01june <daizy0114.becse24@chitkara.edu.in>
2025-02-02 12:27:15 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -7,7 +7,7 @@ int main(){
     scanf("%lf",&n);
     scanf("%c",&c);
     switch(c){
-        case '+': printf("%.2lf\n",n+m);
+        case '+': printf("%.2lf\n",m+n);
         break;
         case '-': printf("%.2lf\n",m-n);
         break;
