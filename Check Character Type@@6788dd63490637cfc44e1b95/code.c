75213983877fb3cc57f388fd0a416be10720c141
daizybedi01june <daizy0114.becse24@chitkara.edu.in>
2025-02-02 14:01:16 +0530

    Solution to Check Character Type

--- Check Character Type@@6788dd63490637cfc44e1b95/code.c
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -6,7 +6,7 @@ int main(){
     if (s=='a' || s=='e' || s=='i' || s=='o' || s=='u'){
         printf("Vowel");
     }
-    else if(s=='b' || s=='c' || s=='d' || s=='f' || s=='g' || s=='h' || s=='i' || s=='j' || s=='k' || s=='l' || s=='m' || s=='n' || s=='p' || s=='q' || s=='r' || s=='s' || s=='t' || s=='u' || s=='v' || s=='w' || s=='x' || s=='y' || s=='z')
+    else if(s=='b' || s=='c' || s=='d' || s=='f' || s=='g' || s=='h' || s=='j' || s=='k' || s=='l' || s=='m' || s=='n' || s=='p' || s=='q' || s=='r' || s=='s' || s=='t' || s=='v' || s=='w' || s=='x' || s=='y' || s=='z')
     printf("Consonant");
     else if(s=='0' || s=='1' || s=='2' || s=='3' || s=='4' || s=='5' || s=='6' || s=='7' || s=='8' || s=='9')
     printf("Digit");
