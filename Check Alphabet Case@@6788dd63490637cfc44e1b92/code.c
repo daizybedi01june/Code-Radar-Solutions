9bb1d3f5c210c0ec241ccd90e109d206035828ba
daizybedi01june <daizy0114.becse24@chitkara.edu.in>
2025-02-02 13:13:09 +0530

    Solution to Check Alphabet Case

--- Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
+++ Check Alphabet Case@@6788dd63490637cfc44e1b92/code.c
@@ -6,7 +6,7 @@ int main(){
     if (s=='A' || s=='B' || s=='C' || s=='D' || s=='E' || s=='F' || s=='G' || s=='H' || s=='I' || s=='J' || s=='K' || s=='L' || s=='M' || s=='N' || s=='O' || s=='P' || s== 'Q' || s=='R' || s=='T' || s=='U' || s=='V' || s=='W' || s=='X' || s=='Y' || s=='Z' || s=='S'){
         printf("Uppercase");
     }
-    else if (s=='z' || s=='b'){
+    else if (s=='z' || s=='b' || s=='a' || s=='c' || s=='d' || s=='e' || s=='f' || s=='g' || s=='h' || s=='i' || s=='j' || s=='k' || s=='l' || s=='m' || s=='n' || s=='o' || s=='p' || s=='q' || s=='r' || s=='s' || s=='t' || s=='u' || s=='v' || s=='w' || s=='x' || s=='y'){
         printf("Lowercase");
     }
     else{
