17a30bea8f76afd0a5a173df2613d6e59185af5e
daizybedi01june <daizy0114.becse24@chitkara.edu.in>
2025-03-27 14:49:24 +0530

    Solution to Selection Sort for Integers

---
+++ Selection Sort for Integers@@67ca7acd1fe08d7207c253bc/code.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+void bubbleSort(int arr[],int n){
+       for (int i=0;i<n-1;i++){
+        int min_pos=i;
+        for (int j=i+1;j<n;j++){
+            if (arr[j]<arr[min_pos]){
+                min_pos=j;
+            }
+        }
+        if (min_pos != i){
+            int temp=arr[i];
+            arr[i]=arr[min_pos];
+            arr[min_pos]=temp;
+        }
+}}
+
+void printArray(int arr[],int n){
+    for (int i=0;i<n;i++){
+        printf("%d",arr[i]);
+    }
+}
